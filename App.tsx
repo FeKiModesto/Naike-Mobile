6f44eb69bfe174903547f5b304be2e68be0107a5
FeKiModesto <fekimodesto04@gmail.com>
2026-08-25 11:54:55 -0300

    feat: configura navegação e integra telas

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Listagem } from './src/screens/listagem';
+import { Detalhe } from './src/screens/Detalhe';
+
+const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Listagem">
+          <Stack.Screen name="Listagem" component={Listagem} options={{ title: 'Naike' }} />
+          <Stack.Screen name="Detalhe" component={Detalhe} options={{ title: 'Detalhe' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
